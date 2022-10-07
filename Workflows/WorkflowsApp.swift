3190dadeb4003a85b136c18c74d95f930e9a7049
Melissa Thai <melissapthai@gmail.com>
2022-10-07 11:31:23 -0700

    Initial Commit

---
+++ Workflows/WorkflowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowsApp.swift
+//  Workflows
+//
+//  Created by Melissa Thai on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
